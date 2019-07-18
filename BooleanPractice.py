db7cfe2944fea382f5bb567104b967ccb12a5eb6
Shayda Moezzi <shaydamoezzi@gmail.com>
2019-07-18 11:34:17 -0700

    test

---
+++ BooleanPractice.py
@@ -0,0 +1,7 @@
+print('Enter your password: ')
+password = input()
+correctpassword = 'correctpassword'
+if password == correctpassword:
+    print('Access granted')
+else:
+    print('Access denied')
